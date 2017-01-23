c9656e2d470f6184954b93e55ba871eba540fc68
Michael Hess <michael.hess@udri.udayton.edu>
2017-01-23 01:38:23 -0500

    first commit

---
+++ wiki.rb
@@ -0,0 +1,39 @@
+require "sinatra"
+
+set :bind, "0.0.0.0"
+
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno:: ENOENT
+  return nil
+end
+
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+	end
+end
+ 
+get "/" do
+  erb :welcome
+end
+
+get "/test" do
+  erb :test
+end
+
+get "/new" do 
+  erb :new
+end
+
+get "/:title" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :show
+end
+
+post "/create" do
+  save_content(params["title"], params["content"])
+  redirect "/#{params["title"]}"
+end
+
